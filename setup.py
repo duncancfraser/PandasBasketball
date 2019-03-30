1327cb639b1a02dbe9ee59d204b4f7fc086ec2e5
duncancfraser <duncancfraser@gmail.com>
2019-03-30 12:49:19 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='PandasBasketball',
+
+    version='0.1',
+
+    description='PandasBasketball is a small module intended to scrape data from basketball reference.',
+
+    url='https://github.com/alfremedpal/PandasBasketball',
+
+    author='Alfredo Medina',
+    author_email='info@amedpal.com',
+
+    packages=find_packages(),
+    install_requires=[
+        'beautifulsoup4==4.7.1',
+        'pandas==0.24.2'
+    ]
+)
